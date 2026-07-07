acd6db531d811e1327c3a170254bbbfb494aa69a
ggbtk <120757678+ggbtk@users.noreply.github.com>
2026-07-07 17:06:35 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,14 @@
 export const siteConfig = {
-  siteName: '컨텐츠이용료현금화',
-  siteUrl: 'https://여기에-pages.dev',
-  siteDescription: '희망티켓에서 컨텐츠이용료현금화 이용 방법, 진행 절차, 유의사항 등을 쉽고 자세하게 안내합니다.',
-  author: '희망티켓',
+  siteName: 'SEO Static Blog',
+  siteUrl: 'https://example.com',
+  siteDescription: '\u0057\u006f\u0072\u0064\u0050\u0072\u0065\u0073\u0073\u0020\uc5c6\uc774\u0020\uc6b4\uc601\ud558\ub294\u0020\ube60\ub974\uace0\u0020\uac00\ubcbc\uc6b4\u0020\u0053\u0045\u004f\u0020\ucd5c\uc801\ud654\u0020\uc815\uc801\u0020\ube14\ub85c\uadf8\uc785\ub2c8\ub2e4\u002e',
+  author: 'Blog Author',
   defaultImage: '/images/sample.jpg',
   naverVerificationCode: '',
-  googleVerificationCode: ''
+  googleVerificationCode: '',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
 };
