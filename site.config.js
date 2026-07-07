e3cb47ae7f75f63f15c9e602a65fd3df151e199c
ggbtk <120757678+ggbtk@users.noreply.github.com>
2026-07-07 16:57:40 +0900

    2

--- site.config.js
+++ site.config.js
@@ -1,8 +1,8 @@
 export const siteConfig = {
-  siteName: 'SEO Static Blog',
-  siteUrl: 'https://example.com',
-  siteDescription: 'WordPress 없이 운영하는 빠르고 가벼운 SEO 최적화 정적 블로그입니다.',
-  author: 'Blog Author',
+  siteName: '컨텐츠이용료현금화',
+  siteUrl: 'https://여기에-pages.dev',
+  siteDescription: '희망티켓에서 컨텐츠이용료현금화 이용 방법, 진행 절차, 유의사항 등을 쉽고 자세하게 안내합니다.',
+  author: '희망티켓',
   defaultImage: '/images/sample.jpg',
   naverVerificationCode: '',
   googleVerificationCode: ''
