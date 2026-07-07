bddc62d8c0381e8e25edcea3451cd25a86eacb71
ggbtk <120757678+ggbtk@users.noreply.github.com>
2026-07-07 17:11:46 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 export const siteConfig = {
-  siteName: 'SEO Static Blog',
+  siteName: '컨텐츠이용료현금화 희망티켓',
   siteUrl: 'https://example.com',
-  siteDescription: '\u0057\u006f\u0072\u0064\u0050\u0072\u0065\u0073\u0073\u0020\uc5c6\uc774\u0020\uc6b4\uc601\ud558\ub294\u0020\ube60\ub974\uace0\u0020\uac00\ubcbc\uc6b4\u0020\u0053\u0045\u004f\u0020\ucd5c\uc801\ud654\u0020\uc815\uc801\u0020\ube14\ub85c\uadf8\uc785\ub2c8\ub2e4\u002e',
-  author: 'Blog Author',
-  defaultImage: '/images/sample.jpg',
+  siteDescription: '컨텐츠이용료현금 컨텐츠이용료미납 컨텐츠이용료현금화 콘텐츠용료현금 콘텐츠이용료현금화 콘텐츠이용료미납 정보이용료미납  정보이용료현금 정보이용료현금화 희망티켓',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/img1.png',
   naverVerificationCode: '',
   googleVerificationCode: '',
   floatingButtons: {
